4ee2e0abc594ba70b2991049c3f8eeb397c9ff9f
Usuario NES <usuario.nes@estudiantes.uco>
2023-02-15 20:53:42 -0500

    15/2/2023 commit

---
+++ src/test/java/com/uco/apiSolveit/ApiSolveitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uco.apiSolveit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSolveitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
