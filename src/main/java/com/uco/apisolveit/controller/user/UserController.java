11624d895d1f5e7a57ee9f576d9799b6ddf9bac2
braianlpe333 <braian0804@gmail.com>
2023-02-20 22:08:38 -0500

    Ejemplo Clase

---
+++ src/main/java/com/uco/apisolveit/controller/user/UserController.java
@@ -0,0 +1,23 @@
+package com.uco.apisolveit.controller.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/rest")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @GetMapping("/usuario")
+    public Person getPerson(@RequestBody String email){
+        return userService.get(email);
+    }
+
+    @PostMapping("/usuario")
+    public Person postPerson(@Valid @RequestBody Person person){
+        return userService.save(person);
+    }
+}
--- src/main/java/com/uco/apisolveit/controller/user/user.java
+++
@@ -1,5 +0,0 @@
-package com.uco.apisolveit.controller.user;
-
-public class user {
-    int a=5;
-}
