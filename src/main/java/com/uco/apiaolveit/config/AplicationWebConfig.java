52ee54b76a67d4f65d4be999c273d00323950a59
juanma1231 <juan.zuluaga4461@uco.net.co>
2023-03-15 21:46:56 -0500

    Implementacion de Seguridad v2.0

--- src/main/java/com/uco/apiaolveit/config/AplicationWebConfig.java
+++ src/main/java/com/uco/apiaolveit/config/AplicationWebConfig.java
@@ -1,12 +1,15 @@
 package com.uco.apiaolveit.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@SuppressWarnings("deprecation")
+@Configuration
 public class AplicationWebConfig{
 
     @Bean
--- src/main/java/com/uco/apiaolveit/service/JwtUserDetailsService.java
+++ src/main/java/com/uco/apiaolveit/service/JwtUserDetailsService.java
@@ -7,10 +7,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 
+@Service
 public class JwtUserDetailsService implements UserDetailsService {
 
     @Autowired
