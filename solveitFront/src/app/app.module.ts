13d692aed92b4c3a6644a29448ad8061f939ce80
Juan Pablo Morales Gomez <84155698+JuanMorales4619@users.noreply.github.com>
2023-05-08 22:14:27 -0500

    Inicios del front

--- solveitFront/src/app/app.module.ts
+++ solveitFront/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CreatePublicationComponent } from './create-publication/create-publication.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CreatePublicationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ solveitFront/src/app/create-publication/create-publication.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-publication',
+  templateUrl: './create-publication.component.html',
+  styleUrls: ['./create-publication.component.css']
+})
+export class CreatePublicationComponent  {
+
+  submit(publication: any){
+    console.log("Form submitted",publication);
+    
+  }
+
+}
