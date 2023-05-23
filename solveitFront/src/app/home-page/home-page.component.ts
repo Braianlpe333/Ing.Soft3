4e38afaec6c3e2bf95f89035a37254d90be5729d
Juan Pablo Morales Gomez <84155698+JuanMorales4619@users.noreply.github.com>
2023-05-22 19:02:39 -0500

    hola

--- solveitFront/src/app/home-page/home-page.component.ts
+++ solveitFront/src/app/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+  product: any;
+  
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+      const headers = { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJob2xhIiwiaWF0IjoxNjg0Nzg5MzI2LCJleHAiOjE2ODQ4MjUzMjZ9.hbvo-LJ8nnxmA7ohjVfrNQdeh6CyvYRWbnFvqSrUYNg' }
+      let response = this.http.get<any>('api/v1/rest/publication', { headers });
+      response.subscribe((data)=>this.product = this.product = data.data
+      );
+  }
 
 }
