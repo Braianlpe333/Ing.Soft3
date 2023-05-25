df2bc6397aa5e08e7e75e722c09769e343655ade
juan <juan.zuluaga4461@uco.net.co>
2023-05-24 22:21:38 -0500

    registro componente

--- solveitFront/src/app/registro/registro.component.ts
+++ solveitFront/src/app/registro/registro.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+export interface User{
+  name :string;
+  lastName: string;
+  password:string;
+  email: string;
+  passwordRepeat:string;
+  phoneNumber:number;
+  employmentField:string;
+}
+
+
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent {
+
+export class RegistroComponent implements OnInit {
+  user!: FormGroup;
+  ngOnInit() {
+    this.user =  new FormGroup({
+      name: new FormControl("", [Validators.required, Validators.minLength(2)]),
+      lastName: new FormControl(""),
+      email : new FormControl(""),
+      password: new FormControl(""),
+      passwordRepeat: new FormControl(""),
+      phoneNumber: new FormControl(0),
+      employmentField: new FormControl("")
+    });
+  }
 
 }
