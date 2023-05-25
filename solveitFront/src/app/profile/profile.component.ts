52e9092bfe17f5982ec45f33699ad428392ee697
abedoyaf18 <60907572+abedoyaf18@users.noreply.github.com>
2023-05-24 20:33:52 -0500

    Vista perfiel y siulacion de json

---
+++ solveitFront/src/app/profile/profile.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../services/profile/profile.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit{
+
+  public idUser:number = 0; // esta variable debe jalar del localstorage el ID del user luego de haberse logueado.
+  public listPostings!: any[]; //aqui se cargara cada post traido del back-end
+  public userName!:String;
+
+  constructor(
+    private profileService: ProfileService
+  ){}
+
+  ngOnInit(): void {
+    this.initData();
+  }
+
+  async initData(){
+    this.userName = "Nombre de usuario";
+    let responseDataUser = await this.profileService.loadUserData(this.idUser).subscribe(res => {
+      this.listPostings = res;
+    });
+  }
+
+  async deleteAccount(){}
+
+  async logOut(){}
+
+}
