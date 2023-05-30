8b8a4327309b8e611b0edfb5dceede2f73acfcd0
juan <juan.zuluaga4461@uco.net.co>
2023-05-29 21:42:03 -0500

    cambiosss

--- solveitFront/src/app/registro/registro.component.ts
+++ solveitFront/src/app/registro/registro.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {HttpClient} from '@angular/common/http'
 export interface User{
   name :string;
   lastName: string;
@@ -31,6 +32,28 @@ export class RegistroComponent implements OnInit {
       phoneNumber: new FormControl(0),
       employmentField: new FormControl("")
     });
+  }
+
+
+
+
+  constructor(private http:HttpClient){}
+  subtmit(user: any){
+    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJob2xhIiwiaWF0IjoxNjg0OTc5NDgxLCJleHAiOjE2ODUwMTU0ODF9.6YTeMKYU0o2ggcjlrrW_7ofrT9bZRiRRDU9vVNm4aTA'
+    const headers = { 'Authorization': 'Bearer '+token}
+    var body1 = {name:"",surname:"",email:"",password:"",phone:"",employmentField:""}
+    body1.name =  user.name;
+    body1.surname = user.lastName;
+    body1.email = user.email;
+    body1.password = user.password;
+    body1.phone = user.phone;
+    body1.employmentField = user.employmentField;
+    let response = this.http.post<any>("/api/v1/rest/user", body1, {headers})
+
+    var body2 = {username:"", password:""}
+    body2.username = user.email;
+    body2.password = user.password;
+    let response2 = this.http.post<any>("/api/v1/rest/user", body1, {headers})
   }
 
 }
